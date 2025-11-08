a9c1c1e9d20def356143447454691d98f2445780
abilash1907 <abilashkbahuleyan@gmail.com>
2025-11-09 00:59:18 +0530

    Netlifydeploy service and convert json service created

--- backend/portfolio-java-services/src/main/java/com/api/portfolio_java_services/controller/PortfolioController.java
+++ backend/portfolio-java-services/src/main/java/com/api/portfolio_java_services/controller/PortfolioController.java
@@ -3,6 +3,8 @@ package com.api.portfolio_java_services.controller;
 import com.api.portfolio_java_services.model.Request;
 import com.api.portfolio_java_services.service.AIService;
 import com.api.portfolio_java_services.service.ContentExtractorService;
+import com.api.portfolio_java_services.service.NetlifyDeployService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,18 +31,20 @@ public class PortfolioController {
     @Autowired
     private final ContentExtractorService contentExtractorService;
     private final AIService aiService;
+    private final NetlifyDeployService netlifyDeployService;
 
 
 
 
 //    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping(value = "/extractContent",consumes = MediaType.MULTIPART_FORM_DATA_VALUE )
-    public ResponseEntity<Map<String, String>> classify(@Valid @NotNull @RequestParam("file") MultipartFile file) {
+    public ResponseEntity<Map<String, String>> classify(@Valid @NotNull @RequestParam("file") MultipartFile file) throws JsonProcessingException {
         Map<String, String> response = new HashMap<>();
         response=contentExtractorService.extractContent(file);
         if(response.containsKey("error")){
             return ResponseEntity.badRequest().body(response);
         } else if (response.containsKey("content")) {
+            response.put("jsonContent", aiService.convertToJson(response.get("content")));
             return ResponseEntity.ok(response);
         }else{
             return ResponseEntity.status(500).body(response);
@@ -48,5 +54,25 @@ public class PortfolioController {
     @PostMapping(value = "/generateWebsiteFromAi",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> generateWebsiteFromAi(@RequestBody Request request) {
         return aiService.generateWebsiteFromAI(request);
+    }
+
+    @PostMapping("/publishWebsite/{siteId}")
+    public ResponseEntity<?> publishWebsiteUsingAi(@PathVariable String siteId) {
+        try {
+            String domain = aiService.generateDomainName(siteId);
+            String url = netlifyDeployService.deploySite(siteId, domain);
+
+            return ResponseEntity.ok(Map.of(
+                    "status", "Success",
+                    "domain", domain,
+                    "publicUrl", url
+            ));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(Map.of(
+                    "status", "Failed",
+                    "message", e.getMessage()
+            ));
+        }
     }
 }
--- backend/portfolio-java-services/src/main/java/com/api/portfolio_java_services/service/AIService.java
+++ backend/portfolio-java-services/src/main/java/com/api/portfolio_java_services/service/AIService.java
@@ -1,5 +1,6 @@
 package com.api.portfolio_java_services.service;
 import com.api.portfolio_java_services.model.Request;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 
 @Slf4j
@@ -18,14 +29,16 @@ public class AIService {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
 
+
+
     public AIService() {
         this.webClient = WebClient.builder()
                 .baseUrl("http://localhost:11434")
                 .build();
+
     }
 
     public Flux<String> generateWebsiteFromAI(Request request) {
-
         Mono<String> responseMono= webClient.post()
                 .uri("/api/chat")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -33,14 +46,114 @@ public class AIService {
                 .bodyValue(request)
                 .retrieve()
                 .bodyToMono(String.class);
+
         return responseMono.flatMapMany(response -> {
             try {
                 JsonNode root = objectMapper.readTree(response);
                 // Extract message.content field
-                return Flux.just(root.path("message").path("content").asText());
+                String htmlContent=root.path("message").path("content").asText().replace("```html", "").replace("```", "");
+                String siteId = UUID.randomUUID().toString();  // generate unique ID
+                saveGeneratedFiles(siteId,htmlContent);
+                return Flux.just(htmlContent+"siteId="+siteId);
             } catch (Exception e) {
                 return Flux.error(new RuntimeException("Error parsing JSON", e));
             }
         });
     }
+
+    private void saveGeneratedFiles(String siteId, String htmlContent) throws IOException {
+        String outputDir = "../../generated-sites/";
+        Path sitePath = Paths.get(outputDir + siteId);
+        Files.createDirectories(sitePath);
+
+        // Save HTML
+        Path indexFile = sitePath.resolve("index.html");
+        Files.writeString(indexFile, htmlContent, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+    }
+
+    public String generateDomainName(String siteId)  {
+        try{
+            // 1️⃣ Read website or resume content
+            Path sitePath = Paths.get("../../generated-sites/" + siteId + "/index.html");
+            String htmlContent = Files.readString(sitePath);
+            log.info("htmlContent="+htmlContent);
+
+            // Create a prompt for the AI
+            String prompt = """
+            Based on the following portfolio or resume HTML content,
+            suggest a short, catchy, professional, and SEO-friendly domain name
+            (2-3 words max, lowercase, separated by hyphens).
+            Do not include protocols (http:// or https://).
+            Return only the domain name (no explanations).
+
+            Example formats:
+            - bright-developer
+            - creative-coder
+            - tech-talent
+            - resume-ai-creator
+
+            HTML content:
+            """ + htmlContent;
+            Map<String, Object> requestBody = Map.of(
+                    "model", "gpt-oss:120b-cloud",
+                    "messages", List.of(Map.of(
+                            "role", "user",
+                            "content", prompt
+                    )),
+                    "stream", false
+            );
+
+            // Call the AI model via Spring AI
+            String response = webClient.post()
+                    .uri("/api/chat")
+                    .header("Content-Type", "application/json")
+                    .bodyValue(requestBody)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+            log.info("response="+response);
+
+            // Post-process: attach your deployment base domain
+            JsonNode root = objectMapper.readTree(response);
+
+            // Extract message.content field
+            String domain=root.path("message").path("content").asText();
+            log.info("domain="+domain);
+            return domain;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String convertToJson(String content) throws JsonProcessingException {
+        String prompt = """
+            Analyze the given content getting from resume file and convert this
+            content into perfect JSON format. I need only code response from your
+            side, no need any other explanation.
+            Content :
+            """ + content;
+        Map<String, Object> requestBody = Map.of(
+                "model", "gpt-oss:120b-cloud",
+                "messages", List.of(Map.of(
+                        "role", "user",
+                        "content", prompt
+                )),
+                "stream", false
+        );
+        // Call the AI model via Spring AI
+        String response = webClient.post()
+                .uri("/api/chat")
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        log.info("response="+response);
+
+        JsonNode root = objectMapper.readTree(response);
+        String jsonContent=root.path("message").path("content").asText().replaceAll("```json|```", "").trim();
+        log.info("jsonContent="+jsonContent);
+        return jsonContent;
+    }
+
 }
